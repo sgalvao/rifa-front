54106b05ac5b4b683fd6e1b7b8788a69860fe118
Silvio - Windows <silvio.gabrielgt2000@hotmail.com>
2023-04-14 12:04:45 -0300

    removed scroll x

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -21,6 +21,7 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     background-color: #fff;
+    overflow-x: hidden;
     font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
 `;
