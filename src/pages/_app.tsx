e7ea8de75c8814dff2f990c7493e438dd4b56196
Silvio - Windows <silvio.gabrielgt2000@hotmail.com>
2023-04-25 19:47:43 -0300

    changed pixel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -51,8 +51,8 @@ export default function App({
                 strategy="afterInteractive"
                 dangerouslySetInnerHTML={{
                   __html: `
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  !function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
                   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
                   if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
                   n.queue=[];t=b.createElement(e);t.async=!0;
@@ -60,7 +60,9 @@ export default function App({
                   s.parentNode.insertBefore(t,s)}(window, document,'script',
                   'https://connect.facebook.net/en_US/fbevents.js');
                   fbq('init', ${fbq.FB_PIXEL_ID});
-                  `,
+                  fbq('track', 'PageView');
+             
+                `,
                 }}
               />
               <Component {...pageProps} />
