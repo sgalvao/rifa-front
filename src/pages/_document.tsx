ff54d6191e6512c2c4b795b3d579e00266be60ae
Silvao <silvio.gabrielgt2000@hotmail.com>
2023-05-03 14:56:57 -0300

    removed unused code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -72,42 +72,6 @@ export default class MyDocument extends Document {
             name="facebook-domain-verification"
             content="c5ji503c1wvzags27zzf6rvmorzh3r"
           />
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-HGE9QGPR9G"
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-HGE9QGPR9G');
-`,
-            }}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `!function (w, d, t) {
-                w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};
-              
-                ttq.load('CGVB4B3C77UE8P0FD3UG');
-                ttq.page();
-              }(window, document, 'ttq');`,
-            }}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:3419668,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-            }}
-          />
         </Head>
         <body>
           <noscript>
